a5c1091773fc077f125298808d1bf14bf5769c4d
Henrik <staranderh@gmail.com>
2024-05-17 10:59:54 +0200

    Fixaxade login korrekt check med admin

--- BlazorBackofficeApp/BlazorBackofficeApp/Services/UserService.cs
+++ BlazorBackofficeApp/BlazorBackofficeApp/Services/UserService.cs
@@ -31,4 +31,59 @@ public class UserService(ApplicationDbContext context, UserManager<ApplicationUs
         return await _context.Subscribers.ToListAsync();
     }
 
+    public async Task<bool> UpdateRole(string userId)
+    {
+        var userToUpdate = await _userManager.FindByIdAsync(userId);
+
+        if(userToUpdate != null)
+        {
+            var existingRoles = await _userManager.GetRolesAsync(userToUpdate);
+
+            string desiredRole = "Admin";
+
+            if (!existingRoles.Contains(desiredRole))
+            {
+                string setRole = "Admin";
+                foreach (var role in existingRoles)
+                {
+                    await _userManager.RemoveFromRoleAsync(userToUpdate, role);
+                }
+
+                
+                await _userManager.AddToRoleAsync(userToUpdate, setRole);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       
+        else
+        {
+            return false;
+        }
+    }
+
+    //var user = await _userManager.GetUserAsync(User);
+    //            if (user != null)
+    //            {
+    //                user.FirstName = viewModel.BasicInfo.FirstName;
+    //                user.LastName = viewModel.BasicInfo.LastName;
+    //                user.Email = viewModel.BasicInfo.EmailAdress;
+    //                user.PhoneNumber = viewModel.BasicInfo.Phone;
+    //                user.Bio = viewModel.BasicInfo.Biography;
+
+    //                var result = await _userManager.UpdateAsync(user);
+    //                if (!result.Succeeded)
+    //                {
+    //                    ModelState.AddModelError("IncorrectValues", "Something went wrong! unable to save data.");
+    //                    ViewData["ErrorMessage"] = "Something went wrong! Unable to save data.";
+    //                }
+    //                if (result.Succeeded)
+    //                {
+    //                    ViewData["SuccessMessage"] = "Data saved successfully";
+    //                }
+    //            }
 }
