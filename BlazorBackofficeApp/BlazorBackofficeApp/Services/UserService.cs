1189a715a9c9655997af7bf60668a12ec9fef031
Henrik <staranderh@gmail.com>
2024-05-23 14:17:03 +0200

    Klar med RUD subscription

--- BlazorBackofficeApp/BlazorBackofficeApp/Services/UserService.cs
+++ BlazorBackofficeApp/BlazorBackofficeApp/Services/UserService.cs
@@ -1,16 +1,20 @@
-﻿using BlazorBackofficeApp.Data;
+﻿using BlazorBackofficeApp.Components.Pages;
+using BlazorBackofficeApp.Data;
 using BlazorBackofficeApp.Data.Entities;
 using BlazorBackofficeApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
 
 namespace BlazorBackofficeApp.Services;
 
-public class UserService(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+public class UserService(ApplicationDbContext context, UserManager<ApplicationUser> userManager, HttpClient httpClient)
 {
     private readonly ApplicationDbContext _context = context;
     private readonly UserManager<ApplicationUser> _userManager = userManager;
-
+    private readonly HttpClient _httpClient = httpClient;
+    
     public async Task<bool> DeleteUser(string userId)
     {
         var userToDelete = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == userId);
@@ -27,11 +31,48 @@ public class UserService(ApplicationDbContext context, UserManager<ApplicationUs
         }
     }
 
-    public async Task<List<SubscribersEntity>> GetAllSubscribersAsync()
+    public async Task<List<SubscibersModel>> GetAllUsersAsync()
     {
-        return await _context.Subscribers.ToListAsync();
-    }
+        HttpClient client = new HttpClient();
 
+        var url = "https://newsletterprovider-silicon-cl.azurewebsites.net/api/GetAllSubscribers?code=QvBLt9AWE0Qc2qxxer8Vv6rOpZ-x7Q0xDwfU4NqDC5yJAzFu_EC08g%3D%3D";
+
+        var response = await client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var subscribers = JsonConvert.DeserializeObject<List<SubscibersModel>>(responseBody)!;
+            return new List<SubscibersModel>(subscribers);
+        }
+        else
+        {
+            return null!;
+        }
+    }
+    public async Task<bool> DeleteSubscriber(string email)
+    {
+        try
+        {
+            var emailObject = new EmailObject { Email = email };
+            var response = await _httpClient.PostAsJsonAsync("https://newsletterprovider-silicon-cl.azurewebsites.net/api/Unsubscribe?code=bSWxf0Zb8QqeQRM0TVMybkHt5y5HgypnbwdEfNKXKP_XAzFu7F3Lzg%3D%3D", emailObject);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+                
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
     public async Task<bool> UpdateRole(string userId)
     {
         var userToUpdate = await _userManager.FindByIdAsync(userId);
