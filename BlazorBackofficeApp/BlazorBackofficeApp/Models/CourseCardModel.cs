90094b1101e7ff7f094aac6293d505b8234a8a29
Henrik <staranderh@gmail.com>
2024-05-27 15:40:45 +0200

    Snart klar med courses

---
+++ BlazorBackofficeApp/BlazorBackofficeApp/Models/CourseCardModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace BlazorBackofficeApp.Models
+{
+    public class CourseCardModel
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsBestSeller { get; set; }
+        public bool IsDigital { get; set; }
+        public List<string>? Categories { get; set; }
+        public string? ImageUri { get; set; }
+        public List<Author>? Authors { get; set; }
+        public Prices? Prices { get; set; }
+        public string? Hours { get; set; }
+        public string? Likes { get; set; }
+        public string? LikesInPercent { get; set; }
+    }
+
+    public class Author
+    {
+        public string? Name { get; set; }
+    }
+
+    public class Prices
+    {
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
